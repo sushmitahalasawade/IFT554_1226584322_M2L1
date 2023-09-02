b3a9bcd09f8591b4033bbce112e1c74e674edd3b
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-01 20:03:09 -0700

    Video 2: Part B  done with setting response header

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const server = httpServer.createServer((req,res) => { // callback function
     const urlParameter = url.parse(req.url,true);
     console.log(urlParameter.query);
     console.log(urlParameter.pathname);
-    res.end(` we received our first request from the client`);
+    if(urlParameter.query.id){
+        //course page
+        if(urlParameter.pathname === "/" || urlParameter.pathname.toLowerCase() === '/courses'){
+            res.writeHead(200,{ // successfull
+                'Content-type':'text/html'
+            });
+            res.end(` we received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id}`);
 
+        }
+  
+    }
+    else{
+        res.writeHead(404,{ // server did not found what you were looking for
+            'Content-type':'text/html'
+        });
+        res.end('resource not found');
+}
+    
 });
 
 //start listning to request
